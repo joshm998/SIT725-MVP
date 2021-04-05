7c416de5b32ab7bdbfd882fbb962d70e92d32e66
Josh Mangiola <joshm982@gmail.com>
2021-04-05 20:45:56 +1000

    Fixes ESLinting Files

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    root: true,
+    extends: ['airbnb-base', 'prettier'],
+    plugins: [],
+    parserOptions: {
+      ecmaVersion: 12
+    },
+    env: {
+      'commonjs': true,
+      'es2021': true,
+      'node': true
+    },
+    rules: {
+    }
+}
